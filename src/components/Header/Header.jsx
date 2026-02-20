6aad10283a092ff81d861186ca8fae0c59dfe082
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-02-20 20:56:01 +0200

    good

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,47 +2,45 @@ import React, { forwardRef } from "react";
 import styles from "./Header.module.scss";
 import { NavLink } from "react-router";
 
+//components
+import Logo from "../Logo/Logo";
+import SearchBar from "../SearchBar/SearchBar";
+
+//assets
+import logo_path from "../../assets/logo.png";
+
+const defaultNavigation = [
+	{ name: "Home", path: "#" },
+	{ name: "Restaurants", path: "movies" },
+	{ name: "Orders", path: "tv-series" },
+	{ name: "Contact", path: "#" },
+	{ name: "Lista mea", path: "#" },
+];
+
 export default function Header({
 	className = "",
-	navbarClassName = "",
-	navbarProps,
-	ref,
+	primaryNavigation = defaultNavigation,
 	...props
 }) {
 	return (
-		<header ref={ref} className={styles.header + ` ${className}`} {...props}>
+		<header className={styles.header + ` ${className}`} {...props}>
 			<nav className={styles.nav}>
-				{props.primaryNavigation ? (
-					<div className={styles.primaryNavigationContainer}>
-						<Logo className={styles.logo + ` ${props.classNameLogo}`} src={logo_path} />
-						<ul className={styles.primaryNavigationList}>
-							{props.primaryNavigation.map((item, index) => (
-								<li className={styles.listItem} key={index}>
-									<NavLink className={styles.link} to={item.path}>
-										{item.name}
-									</NavLink>
-								</li>
-							))}
-						</ul>
-					</div>
-				) : (
+				<div className={styles.left}>
 					<Logo className={styles.logo} src={logo_path} />
-				)}
+					<ul className={styles.primaryNavigationList}>
+						{primaryNavigation.map((item, index) => (
+							<li className={styles.listItem} key={index}>
+								<NavLink className={styles.link} to={item.path}>
+									{item.name}
+								</NavLink>
+							</li>
+						))}
+					</ul>
+				</div>
 
-				{props.secondaryNavigation && (
-					<div className={styles.secondaryNavigation}>
-						<SearchBar />
-						<ul className={styles.secondaryNavigationList}>
-							{props.secondaryNavigation.map((item, index) => (
-								<li className={styles.listItem} key={index}>
-									<NavLink className={styles.link} to={item.path}>
-										{item.name}
-									</NavLink>
-								</li>
-							))}
-						</ul>
-					</div>
-				)}
+				<div className={styles.right}>
+					<SearchBar />
+				</div>
 			</nav>
 		</header>
 	);

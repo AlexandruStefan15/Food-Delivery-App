dd2d9dce3ca1a8d3ccecf73a0bf6d648e023bb48
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-02-24 02:36:42 +0200

    good

--- src/api/restaurantMenuCategories.js
+++ src/api/restaurantMenuCategories.js
@@ -4,7 +4,7 @@ export const useRestaurantFoodCategories = (restaurant) => {
 	return useQuery({
 		queryKey: ["food-categories", restaurant?.id],
 		queryFn: async () => {
-			const allCats = await fetch(`${process.env.REACT_APP_API_URL}/food-categories`).then(
+			const allCategories = await fetch(`${process.env.REACT_APP_API_URL}/food-categories`).then(
 				(res) => {
 					if (!res.ok) {
 						throw new Error("Failed to fetch food categories");
@@ -12,7 +12,7 @@ export const useRestaurantFoodCategories = (restaurant) => {
 					return res.json();
 				},
 			);
-			return allCats.filter((cat) => restaurant.food_categories_ids.includes(cat.id));
+			return allCategories.filter((cat) => restaurant.food_categories_ids.includes(cat.id));
 		},
 		enabled: !!restaurant,
 	});

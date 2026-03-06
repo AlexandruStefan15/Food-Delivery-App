e007685d51e41d34f189c695ad9924c7e825dc90
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-03-06 14:16:37 +0200

    good

---
+++ src/components/sections/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./HeroBanner.module.scss";
+
+export default function HeroBanner() {
+	return (
+		<section className={styles.section}>
+			<div className={styles.container}>Hero Banner</div>
+		</section>
+	);
+}

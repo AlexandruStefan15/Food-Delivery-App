ab2b1bdeb194ccfda57a850e2dc585d64962e4fa
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-03-07 13:59:12 +0200

    good

--- src/components/sections/HeroBanner/HeroBanner.tsx
+++ src/components/sections/HeroBanner/HeroBanner.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./HeroBanner.module.scss";
 
-export default function HeroBanner() {
+//componets
+import SearchBar from "../../SearchBar/SearchBar";
+
+export default function HeroBanner({
+	title = "Cravings delivered to your doorstep.",
+	subtitle = "Order from your favorite local restaurants with ease. Fast, fresh, and reliable.",
+}) {
 	return (
 		<section className={styles.section}>
-			<div className={styles.container}>Hero Banner</div>
+			<div className={styles.container}>
+				<h1 className={styles.title}>{title}</h1>
+				<h3 className={styles.subtitle}>{subtitle}</h3>
+				<SearchBar className={styles.searchBar} />
+			</div>
 		</section>
 	);
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,12 +4,15 @@ import styles from "./Home.module.scss";
 //components
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import HeroBanner from "../../components/sections/HeroBanner/HeroBanner";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
 			<Header />
-			<main className={styles.main}></main>
+			<main className={styles.main}>
+				<HeroBanner />
+			</main>
 			<Footer />
 		</div>
 	);

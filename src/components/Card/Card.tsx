28f7c7d9f832b37886ec24f26a5da04bf617457c
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-03-14 15:03:47 +0200

    good

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,28 +2,38 @@ import React from "react";
 import styles from "./Card.module.scss";
 import { CardProps } from "./Card.types";
 
-export default function Card({ data, variant = "default", className = "" }: CardProps) {
-	if (variant == "circle")
-		return (
-			<div className={styles[`card_${variant}`] + ` ${className}`}>
-				<div className={styles.imgWrapper}>
-					<img className={styles.img} src={data.image.url} alt={data.image.alt}></img>
-				</div>
-				<h3 className={styles.title}>{data.title}</h3>
-			</div>
-		);
+//icons
+import { BsClock } from "react-icons/bs";
+import { MdOutlineDeliveryDining } from "react-icons/md";
 
+export default function Card({ data, variant = "default", className = "", ...props }: CardProps) {
 	return (
-		<div className={styles[`card_${variant}`] + ` ${className}`}>
+		<div className={styles[`card_${variant}`] + ` ${className}`} {...props}>
 			<header className={styles.header}>
-				<div className={styles.imgWrapper}>
-					<img className={styles.img} src={data.image.url} alt={data.image.alt}></img>
-				</div>
+				{data.image && (
+					<div className={styles.imgWrapper}>
+						<img className={styles.img} src={data.image.url} alt={data.image.alt}></img>
+					</div>
+				)}
 			</header>
 			<div className={styles.body}>
 				<h3 className={styles.title}>{data.title}</h3>
+				{data.subtitle && <h4 className={styles.subtitle}>{data.subtitle}</h4>}
 			</div>
-			<footer className={styles.footer}></footer>
+			<footer className={styles.footer}>
+				{data.time && (
+					<span className={styles.time}>
+						<BsClock />
+						<span>{data.time}</span>
+					</span>
+				)}
+				{data.deliveryFee !== undefined && (
+					<span className={styles.deliveryFee}>
+						<MdOutlineDeliveryDining />
+						{data.deliveryFee > 0 ? `$${data.deliveryFee} delivery` : "Free delivery"}
+					</span>
+				)}
+			</footer>
 		</div>
 	);
 }
--- src/components/Card/Card.types.ts
+++ src/components/Card/Card.types.ts
@@ -1,14 +1,18 @@
 export interface CardData {
   id: number;
   title: string;
-  image: {
+  subtitle?: string;
+  image?: {
     url: string;
     alt: string;
   };
+  time?: string;
+  deliveryFee?: number
 }
 
-export interface CardProps {
+export interface CardProps extends React.ComponentPropsWithoutRef<"div"> {
   variant?: "default" | "circle";
   className?: string;
   data: CardData;
+  children?: React.ReactNode;
 }

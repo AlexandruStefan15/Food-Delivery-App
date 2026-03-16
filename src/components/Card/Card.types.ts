9d75ac5a51ca0a52801b6cf687544fafa748de66
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-03-16 04:36:30 +0200

    good

--- src/components/Card/Card.types.ts
+++ src/components/Card/Card.types.ts
@@ -23,7 +23,7 @@ export interface CardData {
 }
 
 export interface CardProps extends React.ComponentPropsWithoutRef<"div"> {
-  variant?: "default" | "circle";
+  variant?: "default" | "circle" | "basic";
   className?: string;
   data: CardData;
   children?: React.ReactNode;
--- src/components/sections/HowItWorks/HowItWorks.tsx
+++ src/components/sections/HowItWorks/HowItWorks.tsx
@@ -11,9 +11,9 @@ export default function HowItWorks() {
 	return (
 		<section className={styles.section}>
 			<div className={styles.container}>
-				<Card data={howItWorks_data[0]} />
-				<Card data={howItWorks_data[0]} />
-				<Card data={howItWorks_data[0]} />
+				<Card data={howItWorks_data[0]} variant="basic" />
+				<Card data={howItWorks_data[0]} variant="basic" />
+				<Card data={howItWorks_data[0]} variant="basic" />
 			</div>
 		</section>
 	);

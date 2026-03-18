c929a4aa213573df3e599a781f56718d7d1785ff
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-03-18 22:35:03 +0200

    good

--- src/api/dishes.ts
+++ src/api/dishes.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import type { Dish } from "../types";
 
 export const useDishes = (restaurantId: number | null) => {
@@ -17,4 +17,42 @@ export const useDishes = (restaurantId: number | null) => {
     },
     enabled: !!restaurantId,
   });
+};
+
+
+export const useAddDish = () => {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: createProject,
+    isPending: isCreating,
+    error: createError,
+  } = useMutation<Dish, Error, Omit<Dish, "id">>({
+    mutationFn: async (newDish): Promise<Dish> => {
+      const res = await fetch("http://localhost:3001/dishes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newDish),
+      });
+
+      if (!res.ok) {
+        throw new Error(`Failed to add dish (Status: ${res.status})`);
+      }
+
+      return res.json();
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["dishes", data.restaurant_id],
+      });
+    },
+  });
+
+  return {
+    createProject,
+    isCreating,
+    createError,
+  };
 };

67b0930134da7504672ed39a94e461e510a4ea28
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-03-23 08:24:38 +0200

    good

--- src/api/dishes.ts
+++ src/api/dishes.ts
@@ -2,57 +2,54 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import type { Dish } from "../types";
 
 export const useDishes = (restaurantId: number | null) => {
-  return useQuery<Dish[], Error>({
-    queryKey: ["dishes", restaurantId],
-    queryFn: async (): Promise<Dish[]> => {
-      const res = await fetch(
-        `http://localhost:3001/dishes?restaurant_id=${restaurantId}`
-      );
+	return useQuery<Dish[], Error>({
+		queryKey: ["dishes", restaurantId],
+		queryFn: async (): Promise<Dish[]> => {
+			const res = await fetch(`http://localhost:3001/dishes?restaurant_id=${restaurantId}`);
 
-      if (!res.ok) {
-        throw new Error(`Failed to fetch dishes (Status: ${res.status})`);
-      }
+			if (!res.ok) {
+				throw new Error(`Failed to fetch dishes (Status: ${res.status})`);
+			}
 
-      return res.json();
-    },
-    enabled: !!restaurantId,
-  });
+			return res.json();
+		},
+		enabled: !!restaurantId,
+	});
 };
 
-
 export const useAddDish = () => {
-  const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-  const {
-    mutate: createProject,
-    isPending: isCreating,
-    error: createError,
-  } = useMutation<Dish, Error, Omit<Dish, "id">>({
-    mutationFn: async (newDish): Promise<Dish> => {
-      const res = await fetch("http://localhost:3001/dishes", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newDish),
-      });
+	const {
+		mutate: createProject,
+		isPending: isCreating,
+		error: createError,
+	} = useMutation<Dish, Error, Omit<Dish, "id">>({
+		mutationFn: async (newDish): Promise<Dish> => {
+			const res = await fetch("http://localhost:3001/dishes", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newDish),
+			});
 
-      if (!res.ok) {
-        throw new Error(`Failed to add dish (Status: ${res.status})`);
-      }
+			if (!res.ok) {
+				throw new Error(`Failed to add dish (Status: ${res.status})`);
+			}
 
-      return res.json();
-    },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({
-        queryKey: ["dishes", data.restaurant_id],
-      });
-    },
-  });
+			return res.json();
+		},
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({
+				queryKey: ["dishes", data.restaurant_id],
+			});
+		},
+	});
 
-  return {
-    createProject,
-    isCreating,
-    createError,
-  };
+	return {
+		createProject,
+		isCreating,
+		createError,
+	};
 };

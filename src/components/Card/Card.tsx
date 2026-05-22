50a320087029c483c32b3dd4ebff8700b976e2e4
Alex Nastase <workplatforms318@gmail.com>
2026-05-22 15:50:04 +0300

    good

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,84 +1,22 @@
 import React from "react";
 import styles from "./Card.module.scss";
-import { CardProps } from "./Card.types";
 
-import { BsClock } from "react-icons/bs";
-import { MdOutlineDeliveryDining } from "react-icons/md";
+export default function Card({ children, className = "", ...props }: React.ComponentPropsWithoutRef<"div">) {
+	return <div>{children}</div>;
+}
 
-export default function Card(props: CardProps) {
-	if (props.variant === "basic") {
-		const { data, className = "", variant, ...divProps } = props;
-
-		return (
-			<div className={`${styles.card_basic} ${className}`} {...divProps}>
-				{data.image && (
-					<div className={styles.imgWrapper}>
-						<img className={styles.img} src={data.image.url} alt={data.image.alt} />
-					</div>
-				)}
-
-				<div className={styles.content}>
-					<h3 className={styles.title}>{data.title}</h3>
-				</div>
-			</div>
-		);
-	}
-
-	if (props.variant === "featured") {
-		const { data, className = "", variant, ...divProps } = props;
-
-		return (
-			<div className={`${styles.card_featured} ${className}`} {...divProps}>
-				<header className={styles.header}>
-					{data.card_image && <img className={styles.img} src={data.card_image} alt="restaurant card image" />}
-				</header>
-
-				<div className={styles.body}>
-					<h3 className={styles.title}>{data.name}</h3>
-
-					{data.cuisine && <h4 className={styles.subtitle}>{data.cuisine}</h4>}
-
-					<div className={styles.metadata}>
-						{data.delivery_time && (
-							<span className={styles.metaItem}>
-								<BsClock />
-								<span>{data.delivery_time}</span>
-							</span>
-						)}
-
-						{data.delivery_fee !== undefined && data.delivery_fee !== null && (
-							<span className={styles.metaItem}>
-								<MdOutlineDeliveryDining />
-								<span>{data.delivery_fee > 0 ? `$${data.delivery_fee} delivery` : "Free delivery"}</span>
-							</span>
-						)}
-					</div>
-				</div>
-			</div>
-		);
-	}
-
-	const { data, className = "", variant, ...divProps } = props;
-
+Card.Title = function CardTitle({ className = "", children, ...props }: React.ComponentPropsWithoutRef<"h2">) {
 	return (
-		<div className={`${styles.card_default} ${className}`} {...divProps}>
-			<header className={styles.header}>
-				{data.icon && (
-					<div className={styles.iconWrapper}>
-						{data.icon.type === "component" ? (
-							<data.icon.component className={styles.icon} />
-						) : (
-							<img src={data.icon.src} alt={data.icon.alt} />
-						)}
-					</div>
-				)}
-			</header>
+		<h2 className={styles.title + ` ${className}`} {...props}>
+			{children}
+		</h2>
+	);
+};
 
-			<div className={styles.body}>
-				<h3 className={styles.title}>{data.title}</h3>
-
-				{data.subtitle && <h4 className={styles.subtitle}>{data.subtitle}</h4>}
-			</div>
+Card.Subtitle = function CardSubtitle({ className = "", children, ...props }: React.ComponentPropsWithoutRef<"div">) {
+	return (
+		<div className={styles.cls + ` ${className}`} {...props}>
+			{children}
 		</div>
 	);
-}
+};

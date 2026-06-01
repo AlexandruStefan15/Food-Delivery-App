ab3af402f00245abfdd88eba7c40569e99e1e9dd
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-06-01 13:54:44 +0300

    good

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -11,35 +11,38 @@ import { RestaurantCardProps, RatingBadgeProps } from "./RestaurantCard.types";
 
 //components
 import Card from "../Card/Card";
+import { NavLink } from "react-router";
 
 export default function RestaurantCard({ data, className = "", ...props }: RestaurantCardProps) {
 	return (
-		<Card className={`${styles.card} ${className}`} {...props}>
-			<header className={styles.header}>
-				{data.card_image && <img className={styles.img} src={data.card_image} alt="restaurant card image" />}
-			</header>
-			<div className={styles.body}>
-				<div className={styles.titleWrapper}>
-					<h3 className={styles.title}>{data.name}</h3>
-					{data.rating && <RatingBadge rating={data.rating} />}
+		<NavLink to="/restaurants/:restaurantId">
+			<Card className={`${styles.card} ${className}`} {...props}>
+				<header className={styles.header}>
+					{data.card_image && <img className={styles.img} src={data.card_image} alt="restaurant card image" />}
+				</header>
+				<div className={styles.body}>
+					<div className={styles.titleWrapper}>
+						<h3 className={styles.title}>{data.name}</h3>
+						{data.rating && <RatingBadge rating={data.rating} />}
+					</div>
+					{data.cuisine && <Card.Subtitle className={styles.subtitle}>{data.cuisine}</Card.Subtitle>}
+					<div className={styles.metadata}>
+						{data.delivery_time && (
+							<div className={styles.metaItem}>
+								<MdAccessTime size={17} color="#6b7280" />
+								<Card.Text>{data.delivery_time}</Card.Text>
+							</div>
+						)}
+						{data.delivery_fee !== undefined && data.delivery_fee !== null && (
+							<div className={styles.metaItem}>
+								<MdOutlineDeliveryDining size={18} color="#6b7280" />
+								<Card.Text>{data.delivery_fee > 0 ? `$${data.delivery_fee} delivery` : "Free delivery"}</Card.Text>
+							</div>
+						)}
+					</div>
 				</div>
-				{data.cuisine && <Card.Subtitle className={styles.subtitle}>{data.cuisine}</Card.Subtitle>}
-				<div className={styles.metadata}>
-					{data.delivery_time && (
-						<div className={styles.metaItem}>
-							<MdAccessTime size={17} color="#6b7280" />
-							<Card.Text>{data.delivery_time}</Card.Text>
-						</div>
-					)}
-					{data.delivery_fee !== undefined && data.delivery_fee !== null && (
-						<div className={styles.metaItem}>
-							<MdOutlineDeliveryDining size={18} color="#6b7280" />
-							<Card.Text>{data.delivery_fee > 0 ? `$${data.delivery_fee} delivery` : "Free delivery"}</Card.Text>
-						</div>
-					)}
-				</div>
-			</div>
-		</Card>
+			</Card>
+		</NavLink>
 	);
 }
 

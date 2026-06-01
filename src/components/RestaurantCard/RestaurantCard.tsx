d1f650d76da24a072c58282984ad7c4f7579153e
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-06-01 13:45:11 +0300

    good

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -44,8 +44,37 @@ export default function RestaurantCard({ data, className = "", ...props }: Resta
 }
 
 function RatingBadge({ rating }: RatingBadgeProps) {
+	const badgeColor = (rating: number) => {
+		switch (true) {
+			case rating > 4:
+				return "#15803d";
+
+			case 3.5 <= rating && rating <= 4.5:
+				return "#e79500";
+
+			default:
+				return "#d70000";
+		}
+	};
+
+	const badgeBackgroundColor = (rating: number) => {
+		switch (true) {
+			case rating > 4:
+				return "#dcfce7";
+
+			case 3.5 <= rating && rating <= 4.5:
+				return "#ffaa0037";
+
+			default:
+				return "#c800002b";
+		}
+	};
+
 	return (
-		<span className={styles.ratingBadge}>
+		<span
+			className={styles.ratingBadge}
+			style={{ color: badgeColor(rating), backgroundColor: badgeBackgroundColor(rating) }}
+		>
 			<IoMdStar size={15.5} />
 			<span className={styles.text}>{rating}</span>
 		</span>

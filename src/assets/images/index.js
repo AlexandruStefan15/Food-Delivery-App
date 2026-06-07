05716615627bf59cd3445d1adc0f5d6f1a17e1d7
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-06-07 04:43:46 +0300

    good

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
+import heroBannerBackground from "./heroBannerBackground.webp";
 
-const images = { logo };
+const images = { logo, heroBannerBackground };
 
 export default images;

4342760b66f012a83cc44d87ffa2268570b2689d
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-06-19 03:56:30 +0300

    good

--- src/pages/Restaurants/Restaurants.tsx
+++ src/pages/Restaurants/Restaurants.tsx
@@ -1,6 +1,19 @@
 import React from "react";
-import styles from "./Restaurants.module.scss"
+import styles from "./Restaurants.module.scss";
+
+//components
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 export default function Restaurants() {
-  return <div>Restaurants</div>;
+	return (
+		<div className={styles.page}>
+			<Header />
+			<main className={styles.main}>
+				{/* <Filters/> */}
+				{/* <RestaurantList/> */}
+			</main>
+			<Footer />
+		</div>
+	);
 }

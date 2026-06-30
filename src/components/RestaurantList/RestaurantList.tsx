e5b331b9109e2c05d01251fa31b2f636680a9ee7
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-06-30 18:19:16 +0300

    good

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -1,8 +1,9 @@
-import React, { ComponentPropsWithoutRef } from "react";
+import React, { ComponentPropsWithoutRef, useState } from "react";
 import styles from "./RestaurantList.module.scss";
 
 //components
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
+import RangeInput from "../RangeInput/RangeInput";
 import Button from "../Button/Button";
 
 //types
@@ -10,23 +11,52 @@ import { Restaurant } from "../../types";
 
 interface RestaurantListProps extends ComponentPropsWithoutRef<"ul"> {
 	restaurants: Restaurant[];
-	showOnly: number;
+	showOnly?: number;
 }
 
 export default function RestaurantList({ restaurants = [], className = "", showOnly = 6 }: RestaurantListProps) {
+	const [showedRestaurants, setShowedRestaurants] = useState(showOnly);
+
 	return (
 		<div className={styles.restaurantListContainer}>
 			<ul className={styles.restaurantList + ` ${className}`}>
 				{restaurants.map(
 					(restaurant, i) =>
-						i < showOnly && (
+						i < showedRestaurants && (
 							<li className={styles.listItem} key={restaurant.id}>
 								<RestaurantCard className={styles.restaurantCard} data={restaurant} />
 							</li>
 						),
 				)}
 			</ul>
-			{showOnly && <Button className={styles.seeMoreBtn}>Load more restaurants</Button>}
+			<footer className={styles.footer}>
+				<div className={styles.counterWrapper}>
+					<p className={styles.text}>
+						Showing {showedRestaurants < restaurants.length ? showedRestaurants : restaurants.length} of{" "}
+						{restaurants.length} restaurants{" "}
+					</p>
+					<RangeInput
+						value={showedRestaurants}
+						min={0}
+						max={restaurants.length}
+						showLabelsElement={false}
+						showValueBubble={false}
+					/>
+				</div>
+				{showOnly && (
+					<Button
+						className={styles.seeMoreBtn}
+						onClick={() => {
+							setShowedRestaurants((prev) => {
+								if (prev + showOnly <= restaurants.length) return prev + showOnly;
+								else return prev;
+							});
+						}}
+					>
+						Load more restaurants
+					</Button>
+				)}
+			</footer>
 		</div>
 	);
 }

f62d87ec0f4f3a2c21a5292cdde486715dc083b7
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-06-30 13:02:01 +0300

    good

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -3,22 +3,30 @@ import styles from "./RestaurantList.module.scss";
 
 //components
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
+import Button from "../Button/Button";
 
 //types
 import { Restaurant } from "../../types";
 
 interface RestaurantListProps extends ComponentPropsWithoutRef<"ul"> {
 	restaurants: Restaurant[];
+	showOnly: number;
 }
 
-export default function RestaurantList({ restaurants = [], className = "" }: RestaurantListProps) {
+export default function RestaurantList({ restaurants = [], className = "", showOnly = 6 }: RestaurantListProps) {
 	return (
-		<ul className={styles.restaurantList + ` ${className}`}>
-			{restaurants.map((restaurant) => (
-				<li className={styles.listItem} key={restaurant.id}>
-					<RestaurantCard className={styles.restaurantCard} data={restaurant} />
-				</li>
-			))}
-		</ul>
+		<div className={styles.restaurantListContainer}>
+			<ul className={styles.restaurantList + ` ${className}`}>
+				{restaurants.map(
+					(restaurant, i) =>
+						i < showOnly && (
+							<li className={styles.listItem} key={restaurant.id}>
+								<RestaurantCard className={styles.restaurantCard} data={restaurant} />
+							</li>
+						),
+				)}
+			</ul>
+			{showOnly && <Button className={styles.seeMoreBtn}>Load more restaurants</Button>}
+		</div>
 	);
 }

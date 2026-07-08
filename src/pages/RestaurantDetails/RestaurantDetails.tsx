7cbea1914e784da1d12f5d459e83f373aae4cc1b
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-07-08 13:21:43 +0300

    good

--- src/pages/RestaurantDetails/RestaurantDetails.tsx
+++ src/pages/RestaurantDetails/RestaurantDetails.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styles from "./RestaurantDetails.module.scss";
 
+//components
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
 export default function RestaurantDetails() {
-	return <div>RestaurantDetails</div>;
+	return (
+		<div className={styles.page}>
+			<Header />
+			<main className={styles.main}>
+				<div className={styles.banner}>
+					<img className={styles.backgroundImg} src={""} alt=""></img>
+					<h2 className={styles.title}></h2>
+					<div className={styles.info}></div>
+				</div>
+			</main>
+			<Footer />
+		</div>
+	);
 }

983e79c89fe466a427b69b0e799315f9e671c5f3
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-08-10 11:47:25 +0300

    good

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -41,7 +41,7 @@ const ItemList = function ({ items, className = "" }: ItemListProps) {
 			<ul className={styles.list}>
 				{items.map((item) => (
 					<li className={styles.listItem}>
-						<DishCard variant="cart" data={item} />
+						<DishCard variant="cart" data={item} key={item.id} />
 					</li>
 				))}
 			</ul>

48467e548f3617f2dabefacd21b7466f16a9facf
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-08-20 14:07:13 +0300

    good

--- src/components/DeliveryAddressCard/DeliveryAddressCard.tsx
+++ src/components/DeliveryAddressCard/DeliveryAddressCard.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import styles from "./DeliveryAddressCard.module.scss";
 
+//types
 import type { DeliveryAddressCardProps } from "./DeliveryAddressCard.types";
 
-export default function DeliveryAddressCard({ data, className = "" }: DeliveryAddressCardProps) {
-	return <div className={styles.card + ` ${className}`}></div>;
+//icons
+import { GrEdit } from "react-icons/gr";
+
+export default function DeliveryAddressCard({
+	data,
+	setSelectedAddress,
+	selectedAddress,
+	className = "",
+}: DeliveryAddressCardProps) {
+	function handleChange(e: ChangeEvent<HTMLInputElement>) {
+		if (e.target.checked) {
+			setSelectedAddress?.(data.id);
+		}
+	}
+
+	return (
+		<label className={`${styles.card} ${className} ${selectedAddress === data.id ? styles.selected : ""}`}>
+			<input
+				type="radio"
+				name="deliveryAddress"
+				className={styles.radioInput}
+				value={data.id}
+				onChange={handleChange}
+			/>
+			<div className={styles.content}>
+				<div className={styles.details}>
+					<p className={styles.name}>{data.label}</p>
+					<p className={styles.address}>{data.street_address}</p>
+				</div>
+				{selectedAddress === data.id && <GrEdit />}
+			</div>
+		</label>
+	);
 }
--- src/components/DeliveryAddressCard/DeliveryAddressCard.types.ts
+++ src/components/DeliveryAddressCard/DeliveryAddressCard.types.ts
@@ -3,4 +3,6 @@ import type { DeliveryAddress } from "../../types/deliveryAddress";
 export interface DeliveryAddressCardProps {
 	data: DeliveryAddress;
 	className: string;
+	setSelectedAddress?: (value: number) => void;
+	selectedAddress?: number;
 }

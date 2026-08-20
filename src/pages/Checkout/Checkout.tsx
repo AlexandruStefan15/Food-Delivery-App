90002699f5ed4c82d35cc05467fb281c5ad0d175
Alex Nastase <alexandru.stefan.nastase@gmail.com>
2026-08-20 22:41:35 +0300

    good

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import styles from "./Checkout.module.scss";
 
+//store
+import { useCartStore } from "../../store/cartStore";
+
+//api
+import { useRestaurants } from "../../api/restaurants";
+
 //components
 import Accordion from "../../components/Accordion/Accordion";
 import DeliveryAddressCard from "../../components/DeliveryAddressCard/DeliveryAddressCard";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import OrderSummary from "../../components/OrderSummary/OrderSummary";
+import { NavLink } from "react-router";
+import Button from "../../components/Button/Button";
 
 export default function Checkout() {
+	const { restaurants } = useRestaurants();
+	const cartItems = useCartStore((state) => state.items);
+
 	return (
 		<div className={styles.page}>
 			<Header />
-			<main className={styles.main}></main>
+			<main className={styles.main}>
+				<Accordion></Accordion>
+				<OrderSummary items={cartItems} restaurants={restaurants}>
+					<Button variant="primary">Place Order</Button>
+				</OrderSummary>
+			</main>
 			<Footer />
 		</div>
 	);
